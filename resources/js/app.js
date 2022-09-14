2892a05929436fea0a70027967e44accaa878e06
yogi.subekti <yogi.subekti@spesolution.com>
2022-09-14 23:23:15 +0700

    feat: add component view

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
 require('./bootstrap');
+
+import '../../vendor/almasaeed2010/adminlte/dist/js/adminlte.min.js';
+import '../../vendor/almasaeed2010/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js';
+
